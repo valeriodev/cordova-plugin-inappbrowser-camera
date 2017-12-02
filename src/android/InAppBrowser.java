6e513ecefde535498dff143a8996eacbed4c74a8
Valeriodev <valeriodev@ymail.com>
2017-12-02 02:00:49 +0100

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -805,7 +805,7 @@ public class InAppBrowser extends CordovaPlugin {
 			    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
 
 			    //cordova.startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
-			    cordova.startActivityForResult(InAppBrowser.this, Intent.createChooser(content, "Select File"), INPUT_FILE_REQUEST_CODE);
+			    cordova.startActivityForResult(InAppBrowser.this, Intent.createChooser(contentSelectionIntent, "Select File"), INPUT_FILE_REQUEST_CODE);
 
 			    return true;
 
@@ -855,7 +855,7 @@ public class InAppBrowser extends CordovaPlugin {
 
 			    // On select image call onActivityResult method of activity
 			    //cordova.startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
-			    cordova.startActivityForResult(InAppBrowser.this, Intent.createChooser(content, "Select File"), FILECHOOSER_RESULTCODE);
+			    cordova.startActivityForResult(InAppBrowser.this, Intent.createChooser(i, "Select File"), FILECHOOSER_RESULTCODE);
 
 
 			}
